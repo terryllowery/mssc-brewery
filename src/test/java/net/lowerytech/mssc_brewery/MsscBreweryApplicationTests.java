84c48c9e6957a2f257d2106d24839cf869828105
Terry Lowery <tl@lowerytech.net>
2025-09-23 20:48:26 -0500

    Init base spring boot application

---
+++ src/test/java/net/lowerytech/mssc_brewery/MsscBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package net.lowerytech.mssc_brewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
